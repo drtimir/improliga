f5757e86025ecdbae516d0ad3a1f583bad00d47e
just-paja <pavel@proboston.net>
2013-02-04 13:44:29 +0100

    Calendar controls
    * Removed next/last year from calendar controls

--- www/lib/template/partial/impro/event/calendar_controls.php
+++ www/lib/template/partial/impro/event/calendar_controls.php
@@ -6,13 +6,11 @@ Tag::div(array("class" => 'calendar-controls'));
 	}
 
 	Tag::div(array("class" => 'l'));
-		echo icon_for('godmode/navi/first', $icon_size, $link_prev_year);
 		echo icon_for('godmode/navi/prev', $icon_size, $link_prev_month);
 	Tag::close('div');
 
 	Tag::div(array("class" => 'r'));
 		echo icon_for('godmode/navi/next', $icon_size, $link_next_month);
-		echo icon_for('godmode/navi/last', $icon_size, $link_next_year);
 	Tag::close('div');
 
 	Tag::div(array("class" => 'mid'));

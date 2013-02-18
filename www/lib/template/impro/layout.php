0ccaf381e3295d5b2588a21bddfcf99a1842725e
just-paja <pavel@proboston.net>
2013-02-18 11:17:53 +0100

    Public pages
    * Added main menu
    * Added basic routes

--- www/lib/template/impro/layout.php
+++ www/lib/template/impro/layout.php
@@ -7,7 +7,6 @@ Tag::html();
 		content_for('styles', 'impro/layout/footer');
 		content_for('styles', 'impro/events');
 		echo content_from('head');
-
 	Tag::close('head');
 
 	Tag::body();
@@ -16,6 +15,14 @@ Tag::html();
 				Tag::div(array("class" => 'logo', "content" =>
 					link_for(Tag::strong(array("class" => 'hidden', "output" => false, "content" => 'Improliga')), '/')
 				));
+
+				Tag::menu(array("class" => 'plain main'));
+					Tag::li(array("content" => link_for(l('impro_menu_home'), '/')));
+					Tag::li(array("content" => link_for(l('impro_menu_about'), '/o-improlize/')));
+					Tag::li(array("content" => link_for(l('impro_menu_teams'), '/tymy/')));
+					Tag::li(array("content" => link_for(l('impro_menu_events'), '/udalosti/')));
+					Tag::li(array("content" => link_for(l('impro_menu_media'), '/media-o-nas/')));
+				Tag::close('menu');
 			Tag::close('div');
 		Tag::close('header');
 
@@ -30,13 +37,13 @@ Tag::html();
 			Tag::div(array("class" => 'container'));
 				Tag::div(array("class" => 'dynamic'));
 					Tag::div(array("class" => 'contact'));
-						echo section_heading(l('impro_contact'), true, 3);
+						echo section_heading(l('impro_contact'), 3);
 						Tag::address(array("content" => l('impro_address')));
 						echo link_for(l('impro_more_contacts'), '/kontakty/');
 					Tag::close('div');
 
 					Tag::div(array("class" => 'partners'));
-						echo section_heading(l('impro_partners'), true, 3);
+						echo section_heading(l('impro_partners'), 3);
 					Tag::close('div');
 					Tag::span(array("class" => 'cleaner', "close" => true));
 				Tag::close('div');

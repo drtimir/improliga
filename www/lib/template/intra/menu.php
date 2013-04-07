176688c7e15286f09c989b36358f240f26b302b1
just-paja <just.paja@gmail.com>
2013-04-08 01:26:10 +0200

    Intra: Main menu
    * New structure for main menu

--- www/lib/template/intra/menu.php
+++ www/lib/template/intra/menu.php
@@ -14,15 +14,15 @@ Tag::div(array("class" => 'menu_left'));
 
 	Tag::menu(array("class" => array('plain', 'main')));
 		Tag::li(array("content" => label_for('godmode/locations/home', 16, '/', l('intra_wall'))));
+		Tag::li(array("content" => label_for('godmode/modules/calendar', 16, '/profile/events/', l('impro_event_list_my'))));
+		Tag::li(array("content" => label_for('godmode/modules/calendar', 16, '/events/create/', l('impro_event_create_new'))));
+	Tag::close('menu');
+
+	Tag::menu(array("class" => array('plain', 'main')));
 		Tag::li(array("content" => label_for('godmode/items/team', 16, '/teams/', l('impro_teams'))));
 		Tag::li(array("content" => label_for('godmode/modules/calendar', 16, '/events/', l('impro_events'))));
 		Tag::li(array("content" => label_for('impro/objects/discussion', 16, '/discussion/', l('intra_discussion'))));
 		Tag::li(array("content" => label_for('impro/actions/download', 16, '/files/', l('intra_file_share'))));
-	Tag::close('menu');
-
-	Tag::menu(array("class" => array('plain', 'main')));
-		Tag::li(array("content" => label_for('godmode/modules/calendar', 16, '/profile/events/', l('impro_event_list_my'))));
-		Tag::li(array("content" => label_for('godmode/modules/calendar', 16, '/events/create/', l('impro_event_create_new'))));
 	Tag::close('menu');
 
 	Tag::menu(array("class" => array('plain', 'main')));

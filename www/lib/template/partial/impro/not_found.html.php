a17c87084332a538fe8468956ba5a256734fa8d3
just-paja <pavel@proboston.net>
2013-05-28 16:32:44 +0200

    [fix] Not found template
    * Fixed not found template

--- www/lib/template/partial/impro/not_found.html.php
+++ www/lib/template/partial/impro/not_found.html.php
@@ -1,27 +1,27 @@
 <?
 
-Tag::div(array("class" => 'page-block'));
+echo div('page-block');
 
-	Tag::div(array("class" => array('block', 'left', 'layout_34')));
-		echo section_heading('Stránka nenalezena');
+	echo div(array('block', 'left', 'layout_34'));
+		echo $ren->heading_layout('Stránka nenalezena');
 		Tag::p(array("content" => 'Stránka, kterou jste hledali neexistuje. Pravděpodobně vám někdo poslal starý nebo špatný odkaz.'));
 		Tag::p(array("content" => 'Ale to přece nevadí, my máme stránek hrozně moc.'));
-		Tag::ul();
-			Tag::li(array("content" => link_for('Události', '/udalosti/').' - Přijďte se podívat na některé z představeí našich týmů'));
-			Tag::li(array("content" => link_for('Týmy', '/tymy/').' - Projďete si seznam našich týmů'));
-			Tag::li(array("content" => link_for('O Improlize', '/o-improlize/').' - Přečtěte si něco o České Improlize'));
-			Tag::li(array("content" => link_for('Kontakty', '/kontakty/').' - Napište nám'));
-		close('ul');
+		echo ul('plain', array(
+			li($ren->link_for('events', 'Události').' - Přijďte se podívat na některé z představeí našich týmů'),
+			li($ren->link_for('teams', 'Týmy').' - Projďete si seznam našich týmů'),
+			li($ren->link_for('about', 'O Improlize').' - Přečtěte si něco o České Improlize'),
+			li($ren->link_for('contacts', 'Kontakty').' - Napište nám'),
+		));
 	close('div');
 
-	Tag::div(array("class" => array('block', 'right', 'layout_14')));
+	echo div(array('block', 'right', 'layout_14'));
 		$foul = \Impro\Foul::get_random();
 
-		echo heading($foul->name);
+		echo $ren->heading($foul->name);
 		echo div('desc', $foul->desc);
 
-		echo div('link', 'Najděte si další '.link_for('fauly', '/o-improlize/fauly/').'.');
+		echo div('link', 'Najděte si další '.$ren->link_for('fouls', 'fauly').'.');
 	close('div');
 
-	Tag::span(array("class" => 'cleaner', "close" => true));
+	echo span('cleaner', '');
 close('div');

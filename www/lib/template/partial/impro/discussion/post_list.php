2d46b507ab967b5d5b303ad616d774bc1d44c424
just-paja <pavel@proboston.net>
2013-06-13 11:10:01 +0200

    Styles
    * Fixed discussion styles
    * Added class link-profile to user profile link

--- www/lib/template/partial/impro/discussion/post_list.php
+++ www/lib/template/partial/impro/discussion/post_list.php
@@ -17,11 +17,11 @@ echo div('discussion post_list');
 		foreach ($posts as $post) {
 			echo li(array(
 				div('avatar', Impro\User::avatar($ren, $post->author)),
-				div('author', Impro\User::link($ren, $post->author)),
-				div('text', to_html($ren, $post->text)),
-				div('footer', array(
-					$ren->link('#post_'.$post->id, $locales->format_date($post->created_at, 'human'), array("class" => 'date'))
+				div('author', array(
+					Impro\User::link($ren, $post->author),
+					$ren->link('#post_'.$post->id, $locales->format_date($post->created_at, 'human'), array("class" => 'link-date'))
 				)),
+				div('text', $post->text ? to_html($ren, $post->text):'<p class="gray">Prázdný příspěvek</p>'),
 			), 'post', 'post_'.$post->id);
 		}
 

505b540f37945410d04ab653c09ae7c22e1253b2
just-paja <pavel@proboston.net>
2013-06-18 11:40:46 +0200

    [fix] Team template
    * Fixed team template classes

--- www/lib/template/intra/team.php
+++ www/lib/template/intra/team.php
@@ -1,20 +1,21 @@
 <?
 
-echo div(array('block', 'left', 'layout_11', 'content'));
-	$ren->slot('head');
+echo div('team-layout');
+	echo div(array('block', 'left', 'layout_11', 'content'));
+		$ren->slot('head');
+	close('div');
+
+	echo div(array('block', 'left', 'layout_11', 'content'));
+		$ren->slot('big');
+	close('div');
+
+	echo div(array('block', 'left', 'layout_58'));
+		$ren->slot('left');
+	close('div');
+
+	echo div(array('block', 'right', 'layout_38'));
+		$ren->slot('right');
+	close('div');
+
+	$ren->slot();
 close('div');
-
-echo div(array('block', 'left', 'layout_11', 'content'));
-	$ren->slot('big');
-close('div');
-
-echo div(array('block', 'left', 'layout_58'));
-	$ren->slot('left');
-close('div');
-
-echo div(array('block', 'right', 'layout_38'));
-	$ren->slot('right');
-close('div');
-
-$ren->slot();
-

3e7d2d9b522085e1946d0c18093e01c6a86c1d19
just-paja <pavel@proboston.net>
2013-06-25 11:41:49 +0200

    Intranet, input decoration
    * Added input decoration to intranet

--- www/lib/template/intra/layout.php
+++ www/lib/template/intra/layout.php
@@ -13,61 +13,14 @@ echo html($locales->get_lang());
 
 		$renderer->content_for("head", '<script type="text/javascript">'.implode('', $script).'</script>');
 
-		$ren->content_for('styles', 'pwf/elementary');
-		$ren->content_for('styles', 'pwf/calendar');
-		$ren->content_for('styles', 'pwf/browser_control');
-		$ren->content_for("styles", "pwf/form");
-		$ren->content_for("styles", "pwf/form/datetimepicker");
-		$ren->content_for("styles", "pwf/form/datepicker");
-		$ren->content_for("styles", "pwf/form/timepicker");
-		$ren->content_for("styles", "pwf/form/picker_image");
-		$ren->content_for("styles", "pwf/form/autocompleter");
-		$ren->content_for("styles", "pwf/form/rte");
-		$ren->content_for("styles", "pwf/form/tabs");
-		$ren->content_for("styles", "form/search_tool");
-		$ren->content_for("styles", "calendar");
-
-		$ren->content_for('styles', 'lib/fancybox');
-		$ren->content_for('styles', 'intra/layout');
-		$ren->content_for('styles', 'intra/common');
-		$ren->content_for('styles', 'intra/header');
-		$ren->content_for('styles', 'intra/menu_left');
-		$ren->content_for('styles', 'intra/calendar');
-		$ren->content_for('styles', 'intra/news');
-		$ren->content_for('styles', 'intra/events');
-		$ren->content_for('styles', 'intra/discussions');
-		$ren->content_for('styles', 'intra/forms');
-		$ren->content_for('styles', 'intra/teams');
-		$ren->content_for('styles', 'impro/team_comments');
-		$ren->content_for('styles', 'intra/profile');
-		$ren->content_for('styles', 'intra/rte');
-		$ren->content_for('styles', 'intra/requests');
-		$ren->content_for('styles', "intra/reporter");
-
-		$ren->content_for('scripts', 'lib/browser');
-		$ren->content_for('scripts', 'lib/html5');
-		$ren->content_for('scripts', 'lib/jquery/fancybox');
-		$ren->content_for('scripts', 'pwf/browser_control');
-		$ren->content_for('scripts', 'pwf/locales');
-		$ren->content_for('scripts', 'pwf/icon');
-		$ren->content_for('scripts', 'pwf/form/tab_manager');
-		$ren->content_for('scripts', 'pwf/lib/rte');
-		$ren->content_for('scripts', 'pwf/form/date_picker');
-		$ren->content_for('scripts', 'pwf/form/time_picker');
-		$ren->content_for('scripts', 'pwf/form/autocompleter');
-		$ren->content_for('scripts', 'pwf/form/location_picker');
-		//~ $ren->content_for('scripts', 'pwf/form/jquery.gmap');
-		$ren->content_for('scripts', 'pwf/form/gps');
-		$ren->content_for('scripts', "pwf/godmode/relman");
-		$ren->content_for('scripts', "site/reporter");
-		$ren->content_for('scripts', 'site/global');
-		$ren->content_for('scripts', 'site/intranet');
+		$ren->content_for('styles',  "site/lib/intranet");
+		$ren->content_for('scripts', "site/lib/intranet");
 
 		echo $ren->content_from('head');
 	close('head');
 
 	Tag::body();
-		echo htmlheader();
+		echo htmlheader('offset-y');
 			echo div('page-block');
 				echo div('logo', $ren->link_for('home', span('hidden', 'Intranet')));
 				echo div('reporter', '');

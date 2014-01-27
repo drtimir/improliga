8201247b55e374a429bb076e6c9db9117c034283
just-paja <pavel@proboston.net>
2014-01-27 10:30:51 +0100

    [fix] Partners, image API

--- lib/template/partial/impro/static/partners.html.php
+++ lib/template/partial/impro/static/partners.html.php
@@ -5,7 +5,7 @@ echo ul('plain');
 	foreach ($partners as $partner) {
 		echo li($ren->link_ext($partner->site, Stag::span(array(
 			"class" => 'icon isize-32',
-			"style" => 'background-image:url('.$partner->image->thumb_trans(32,32).'); height:32px;',
+			"style" => 'background-image:url('.$partner->image->thumb(32,32).'); height:32px;',
 			"title" => $partner->name,
 			"content" => $partner->name,
 		))));

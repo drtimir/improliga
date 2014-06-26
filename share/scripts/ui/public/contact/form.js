99875ae35349cb9e827c91bf2738faba6c7b3fcd
just-paja <pavel@proboston.net>
2014-06-26 10:43:48 +0200

    Contact form inputs

--- share/scripts/ui/public/contact/form.js
+++ share/scripts/ui/public/contact/form.js
@@ -7,6 +7,9 @@ pwf.rc('ui.contact.form', {
 
 
 	'proto':{
+		'fade_in':250,
+		'fade_out':250,
+
 		'types':[
 			{
 				'ident':'novice',
@@ -48,7 +51,9 @@ pwf.rc('ui.contact.form', {
 				.append(el.menu)
 				.append(el.form);
 
-			el.form.hide();
+			el.form
+				.hide()
+				.addClass('cf-handler');
 		},
 
 
@@ -68,12 +73,14 @@ pwf.rc('ui.contact.form', {
 			}).load();
 
 			for (var i = 0; i < types.length; i++) {
-				var type_el = pwf.jquery.div('cf-menu-item cf-menu-' + types[i].ident);
+				var
+					type_el = pwf.jquery.div('cf-menu-item cf-menu-' + types[i].ident),
+					ctx = {'ctrl':this, 'proto':proto, 'type':types[i]};
 
 				type_el
 					.appendTo(el.items)
 					.append(pwf.jquery.span('label').html(types[i].label))
-					.bind('click', {'proto':proto, 'type':types[i]}, proto('callbacks').select_type);
+					.bind('click', ctx, proto('callbacks').select_type);
 			}
 		},
 
@@ -81,25 +88,82 @@ pwf.rc('ui.contact.form', {
 		'construct_form':function(proto) {
 			proto.storage.form = pwf.create('form', {
 				'parent':this.get_el('form'),
+				'heading':'Napište nám!',
 				'elements':[
 					{
-						'name':'email',
-						'type':'email',
-						'label':'E-Mail'
+						"name":"inputs",
+						'type':'inputs',
+						'element':'container',
+						'elements':[
+							{
+								'name':'reason',
+								'type':'hidden'
+							},
+							{
+								'name':'email',
+								'type':'email',
+								'label':'E-mail'
+							},
+							{
+								'name':'text',
+								'type':'textarea',
+								'label':'Zpráva'
+							}
+						]
 					},
 					{
-						'name':'text',
-						'type':'textarea',
-						'label':'Co nám chcete sdělit?'
+						"name":"buttons",
+						'type':'buttons',
+						'element':'container',
+						'elements':[
+							{
+								'element':'button',
+								'name':'text',
+								'type':'button',
+								'label':'Rozmyslel jsem si to',
+								'click':function(ctrl, proto) {
+									return function() {
+										proto('show_menu');
+									}
+								}(this, proto)
+							},
+							{
+								'element':'button',
+								'name':'text',
+								'type':'submit',
+								'label':'Odeslat'
+							}
+						]
 					}
 				]
 			});
+		},
 
+
+		'show_form':function(proto)
+		{
+			this.get_el('form').stop(true).fadeIn(proto('fade_in'));
+			this.get_el('menu').stop(true).fadeOut(proto('fade_out'));
+		},
+
+
+		'show_menu':function(proto)
+		{
+			this.get_el('form').stop(true).fadeOut(proto('fade_out'));
+			this.get_el('menu').stop(true).fadeIn(proto('fade_in'));
+		},
+
+
+		'select_type':function(proto, type)
+		{
+			proto.storage.form.get_input('reason').val(type.ident);
+			proto('show_form');
 		},
 
 
 		'callbacks':{
 			'select_type':function(e) {
+				e.data.proto('select_type', e.data.type);
 			}
 		}
 	},

1011e5f0f6c3526d76e5f5b0430bf723a5fea808
just-paja <pavel@proboston.net>
2014-06-26 16:15:52 +0200

    Nice team list

--- share/scripts/ui/list/team.js
+++ share/scripts/ui/list/team.js
@@ -13,10 +13,39 @@ pwf.rc('ui.list.team', {
 		},
 
 		'construct':function(proto) {
-			var el = this.get_el().create_divs(['name', 'info', 'location', 'time']);
-			el.info.append(el.location).append(el.time);
+			var
+				el = this.get_el().create_divs(['image', 'data', 'name', 'info', 'city', 'time', 'cleaner']),
+				image = this.get('image');
 
-			el.name.html(pwf.jquery('<a/>').html(this.get('name')).attr('href', '/predstaveni/' + this.get('id')));
+			el.data.append(el.name).append(el.info);
+			el.info.append(el.city).append(el.time);
+
+			el.city.html(this.get('city'));
+
+			if (!image) {
+				image = {
+					'name':'logo.png',
+					'url':'<pixmap(logo.png)>'
+				};
+			}
+
+			el.image.html(pwf.thumb.create(image.name, el.image.width() + 'x' + el.image.height()));
+			el.name.html(pwf.jquery('<a/>').html(this.get('name')).attr('href', this.get_url()));
+			this.get_el().bind('click', this, proto('callbacks').navigate);
+		},
+
+
+		'callbacks':{
+			'navigate':function(e) {
+				document.location = e.data.get_url();
+			}
+		}
+	},
+
+
+	'public':{
+		'get_url':function() {
+			return '/tymy/' + this.get('item').get_seoname();
 		}
 	}
 });
--- share/scripts/ui/public/teams/teams.js
+++ share/scripts/ui/public/teams/teams.js
@@ -6,9 +6,20 @@ pwf.rc('ui.teams.teams', {
 			'heading':'Seznam týmů',
 			'model':'Impro::Team',
 			'draw':'ui.list.team',
-			'per_page':50,
+			'per_page':20,
+			'ui_filters':[
+				{
+					'name':'search',
+					'type':'text',
+					'placeholder':'Vyhledat',
+					'attrs':['name', 'city']
+				}
+			],
 			'filters':[
 				{'attr':'visible', 'type':'exact', 'exact':true}
+			],
+			'sort':[
+				{'attr':'name'}
 			]
 		}
 	}

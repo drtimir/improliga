502a1274af1f10b40cb8932311692ab4ad4a33b3
just-paja <pavel@proboston.net>
2014-06-30 18:30:33 +0200

    Short desc can be null

--- lib/class/impro/event.php
+++ lib/class/impro/event.php
@@ -26,7 +26,7 @@ namespace Impro
 			"team_away"     => array('belongs_to', "model" => 'Impro\Team', "is_null" => true),
 			"location"      => array('belongs_to', "model" => 'System\Location', "is_null" => true),
 			"image"         => array('image', "default" => 'share/pixmaps/logo_original.png'),
-			"desc_short"    => array('html',  "default" => ''),
+			"desc_short"    => array('html',  "default" => '', 'is_null' => true),
 			"desc_full"     => array('html',  "default" => ''),
 			"start"         => array('date'),
 			"start_time"    => array('time', "is_null" => true),

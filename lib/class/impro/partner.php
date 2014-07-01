388e8e248bccd300f66e21ac92d632fbcd88096d
just-paja <pavel@proboston.net>
2014-07-01 15:31:17 +0200

    Partners, modernize attrs

--- lib/class/impro/partner.php
+++ lib/class/impro/partner.php
@@ -2,15 +2,21 @@
 
 namespace Impro
 {
-	class Partner extends \System\Model\Database
+	class Partner extends \System\Model\Perm
 	{
 		protected static $attrs = array(
 			"name"    => array('varchar'),
-			"desc"    => array('text'),
+			"desc"    => array('html'),
 			"site"    => array('url'),
 			"image"   => array('image'),
 			"clicked" => array('int', "is_unsigned" => true),
 			"visible" => array('bool'),
+		);
+
+
+		protected static $access = array(
+			'schema' => true,
+			'browse' => true
 		);
 
 

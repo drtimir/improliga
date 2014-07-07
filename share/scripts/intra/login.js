fcb57c9f1643542ec4c417f80d0f708f5ae0575f
just-paja <pavel@proboston.net>
2014-07-07 17:40:04 +0200

    Intra, login form, make refresh

--- share/scripts/intra/login.js
+++ share/scripts/intra/login.js
@@ -2,40 +2,41 @@ pwf.wi(['form'], function()
 {
 	var get_loader = function(form)
 	{
-		return form.el().find('.loader-tiny');
+		return form.get_el().find('.loader-tiny');
 	};
 
 
 	pwf.queue.on('login_before_send', function(event) {
 		var
 			loader = pwf.jquery.div('loader-tiny'),
-			group = event.response.el().find('.form-group-typed-buttons'),
+			group = event.response.get_el().find('.form-group-typed-buttons'),
 			button = group.find('button');
 
 		group.find('.err').remove();
 		loader.hide().insertAfter(button).fadeIn(250);
 	}, null, true);
 
-	pwf.queue.on('login_error', function(event) {
-		var
-			loader = get_loader(event.response.form),
-			msg = pwf.jquery.div('err').html(pwf.locales.trans(event.response.err));
 
-		msg.hide().insertAfter(loader);
-		loader.fadeOut(150, function(loader, msg) {
+	pwf.queue.on('login_error', function(event) {
+		var loader = get_loader(event.response.form);
+
+		event.response.form.get_el('errors').html(pwf.locales.trans(event.response.error));
+
+		loader.fadeOut(150, function(loader) {
 			return function() {
 				loader.remove();
-				msg.fadeIn(150);
 			};
-		}(loader, msg));
+		}(loader));
 	}, null, true);
+
 
 	pwf.queue.on('login_ready', function(event) {
 		var loader = get_loader(event.response.form);
+
 		loader.fadeOut(250, pwf.jquery.callback_remove);
 
-		if (typeof event.response.data.redirect != 'undefined') {
-			document.location = event.response.data.redirect;
+		if (typeof event.response.response.data.redirect != 'undefined') {
+			document.location = event.response.response.data.redirect;
 		} else document.location = '/';
 
 	}, null, true);

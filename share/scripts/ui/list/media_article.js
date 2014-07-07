924d58327659e739062db7cdecc6557c80460074
just-paja <pavel@proboston.net>
2014-07-07 15:55:32 +0200

    Links, articles

--- share/scripts/ui/list/media_article.js
+++ share/scripts/ui/list/media_article.js
@@ -15,9 +15,16 @@ pwf.rc('ui.list.media_article', {
 
 		'construct':function(proto) {
 			var el = this.get_el().create_divs(['name', 'publisher', 'date']);
+
 			el.name.html(this.get('name'));
 			el.publisher.html(this.get('publisher'));
 			el.date.html(this.get('date').format('D.M.YYYY'));
+
+			el.bind('click', function(ctrl) {
+				return function(e) {
+					window.open(ctrl.get('url'));
+				};
+			}(this));
 		}
 	}
 });

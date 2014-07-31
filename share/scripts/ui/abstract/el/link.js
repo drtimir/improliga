da6be43d0d536f210f96ec621aa9f39866665014
just-paja <pavel@proboston.net>
2014-07-31 14:14:51 +0200

    [fix] Link

--- share/scripts/ui/abstract/el/link.js
+++ share/scripts/ui/abstract/el/link.js
@@ -11,20 +11,28 @@ pwf.rc('ui.abstract.el.link', {
 	},
 
 
-	'init':function()
-	{
-		this.get_el()
-			.attr('href', pwf.dispatcher.url(this.get('path'), this.get('params')))
-			.html(this.get('title'))
-			.bind('click', this, function(e) {
-				e.preventDefault();
+	'proto':{
+		'el_attached':function(proto)
+		{
+			proto('construct');
+		},
 
-				e.data.get_el().trigger('navigate', {
-					'origin':e.data,
-					'name':'navigate',
-					'title':e.data.get_el().text(),
-					'url':e.data.get_el().attr('href')
+
+		'construct':function(proto)
+		{
+			this.get_el()
+				.attr('href', pwf.dispatcher.url(this.get('path'), this.get('params')))
+				.html(this.get('title'))
+				.bind('click', this, function(e) {
+					e.preventDefault();
+
+					e.data.get_el().trigger('navigate', {
+						'origin':e.data,
+						'name':'navigate',
+						'title':e.data.get_el().text(),
+						'url':e.data.get_el().attr('href')
+					});
 				});
-			});
+		}
 	}
 });

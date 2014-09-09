3330e19478b629dc63b620a6fb1ba88e5431053c
just-paja <pavel@proboston.net>
2014-09-09 13:49:20 +0200

    Link optional propagate

--- share/scripts/ui/link.js
+++ share/scripts/ui/link.js
@@ -9,6 +9,7 @@ pwf.rc('ui.link', {
 			'deep':false,
 			'title':null,
 			'params':{},
+			'propagate':true,
 			'event':'navigate'
 		}
 	},
@@ -30,16 +31,7 @@ pwf.rc('ui.link', {
 			el
 				.attr('href', url)
 				.addClass(this.get('cname'))
-				.bind('click', this, function(e) {
-					e.preventDefault();
-
-					e.data.get_el().trigger(e.data.get('event'), {
-						'origin':e.data,
-						'name':e.data.get('event'),
-						'title':e.data.get_el().text(),
-						'url':e.data.get_el().attr('href')
-					});
-				});
+				.bind('click', this, proto('callbacks').open);
 
 			if (this.get('title')) {
 				el.html(pwf.locales.trans(this.get('title')));
@@ -47,6 +39,25 @@ pwf.rc('ui.link', {
 
 			if (url == pwf.dispatcher.get_path() || (this.get('deep') && pwf.dispatcher.get_path().indexOf(url) === 0)) {
 				el.addClass('active');
+			}
+		},
+
+
+		'callbacks':
+		{
+			'open':function(e) {
+				e.preventDefault();
+
+				if (!e.data.get('propagate')) {
+					e.stopPropagation();
+				}
+
+				e.data.get_el().trigger(e.data.get('event'), {
+					'origin':e.data,
+					'name':e.data.get('event'),
+					'title':e.data.get_el().text(),
+					'url':e.data.get_el().attr('href')
+				});
 			}
 		}
 	}
